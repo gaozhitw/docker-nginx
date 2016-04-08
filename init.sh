bb22638e0d46d1e2bd76d0626653b30ed4a63025
gaozhitw <gaozhi.tw@gmail.com>
2016-04-08 10:10:44 +0800

    add dynamic modules replace

--- init.sh
+++ init.sh
@@ -4,4 +4,8 @@ if [ ! "$(ls -A /etc/nginx)" ]; then
 	cp -R $HOME/nginx-default-conf/* /etc/nginx
 fi
 
+if [ ! "$(ls -A /etc/nginx/modules)" ]; then
+    cp -R $HOME/nginx-default-conf/modules /etc/nginx/modules
+fi
+
 exec nginx
